115473dcc2fab002d1c9b37e8b1fea18d1089bb7
rcampion <campion.richard@gmail.com>
2019-07-06 17:09:30 -0400

    Initial Commit

--- src/main/java/com/rkc/zds/Application.java
+++ src/main/java/com/rkc/zds/Application.java
@@ -2,18 +2,20 @@ package com.rkc.zds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@ComponentScan("com.rkc.zds")
 @SpringBootApplication
-@RestController
+// @RestController
 public class Application {
-
+/*
     @RequestMapping("/")
     public String home() {
         return "Hello Docker World";
     }
-
+*/
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
